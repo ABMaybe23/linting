4711296fa0bb88d56e9acedf390db182c597f268
ABMaybe23 <99318421+ABMaybe23@users.noreply.github.com>
2022-03-09 10:10:20 +0300

    Update factorial.py

--- src/factorial.py
+++ src/factorial.py
@@ -6,7 +6,10 @@ def factorial(num):
     num (int): Integer for which factorial is requested
     returns (int): Returns Factorial
     """
+
+
     if num in (0, 1):
         return 1
     return num * factorial(num-1)
+
 print(factorial(5))
